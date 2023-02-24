b79c83fbc7fca4f9a55398e6524a2ca74bc034ce
connor <connordanielfrank@gmail.com>
2023-02-24 17:21:58 +1300

    dev: Begin setting up models

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const createError = require('http-errors');
+const dotenv = require('dotenv');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+
+dotenv.config();
+const { MONGO_DB, MONGO_USER, MONGO_PASSWORD } = process.env;
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+mongoose.set('strictQuery', false);
+const mongoDB = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster1.i1gjaks.mongodb.net/${MONGO_DB}?retryWrites=true&w=majority`;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 app.use(logger('dev'));
 app.use(express.json());
